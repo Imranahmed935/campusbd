0fd1d22859e62fbc3e4b5038eabf7d148633d9b4
Imranahmed935 <imrantahir9918@gmail.com>
2026-02-09 02:07:24 +0600

    prisma error solved

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,13 @@ import { app } from "./app";
 
 dotenv.config();
 
+const port = process.env.PORT || 5000;
+
 async function runServer() {
   try {
     let server = http.createServer(app);
     server.listen(process.env.PORT , () => {
-      console.log(`CampusBd Server running on port | ${process.env.PORT }`);
+      console.log(`CampusBd Server running on port | ${port}`);
     });
   } catch (error) {
     console.error("❌ Server failed to start", error);
